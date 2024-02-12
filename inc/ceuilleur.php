f5b1fd5f041246999bbd8a5d158002fcda6efcdf
NicolasMaku <108606635+NicolasMaku@users.noreply.github.com>
2024-02-12 18:39:48 +0300

    Finition des crud

--- inc/ceuilleur.php
+++ inc/ceuilleur.php
@@ -7,14 +7,15 @@
     }
 
     function ceuilleur_insert($nom,$genre,$dateNaissance) {
-        $parcelle = [
-            "surface" => $surface,
-            "id_variete_the" => $id_variete_the
+        $ceuilleur = [
+            "nom" => $nom,
+            "genre" => $genre,
+            "date_naissance" => $dateNaissance
         ];
 
         try {
-            $crud = new Crud('the_parcelle',connect());
-            $crud->insert($parcelle);
+            $crud = new Crud('the_ceuilleur',connect());
+            $crud->insert($ceuilleur);
 
             return true;
 
@@ -23,14 +24,30 @@
         }
     }
 
-    function ceuilleur_modify() {
+    function ceuilleur_modify($id,$nom,$genre,$dateNaissance) {
+        $ceuilleur = [
+            "nom" => $nom,
+            "genre" => $genre,
+            "date_naissance" => $dateNaissance
+        ];
 
+        try {
+            $crud = new Crud('the_ceuilleur',connect());
+            $crud->update($id,$ceuilleur);
+
+            return true;
+
+        }catch (Exception $exception){
+            return $exception->getMessage();
+        }
     }
 
-    function ceuilleur_delete() {
-
+    function ceuilleur_delete($id) {
+        $crud = new Crud('the_ceuilleur',connect());
+        $crud->delete($id);
     }
 
 
+//    ceuilleur_insert("Kevin","Homme","2018-12-4");
 //    var_dump(getAll());
 ?>
---
+++ inc/salaire.php
@@ -0,0 +1,43 @@
+<?php
+    include_once "base.php";
+    include_once "Crud.php";
+    function salaire_insertion($id_ceuilleur,$montant) {
+        $salaire = [
+            "id_ceuilleur" => $id_ceuilleur,
+            "montant" => $montant
+        ];
+
+        try {
+            $crud = new Crud('the_salaire',connect());
+            $crud->insert($salaire);
+
+            return true;
+
+        }catch (Exception $exception){
+            return $exception->getMessage();
+        }
+    }
+
+    function salaire_modify($id,$id_ceuilleur,$montant) {
+        $salaire = [
+            "id_ceuilleur" => $id_ceuilleur,
+            "montant" => $montant
+        ];
+
+        try {
+            $crud = new Crud('the_salaire',connect());
+            $crud->update($id,$salaire);
+
+            return true;
+
+        }catch (Exception $exception){
+            return $exception->getMessage();
+        }
+    }
+
+    function salaire_getAll() {
+        $crud = new Crud('the_salaire',connect());
+        return $crud->lister(); 
+    }
+
+?>
