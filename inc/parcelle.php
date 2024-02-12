cd4d5d0dc04788e7ab151fe43eb4caf5eca85d67
NicolasMaku <108606635+NicolasMaku@users.noreply.github.com>
2024-02-13 00:01:20 +0300

    reparation parcelle

--- inc/parcelle.php
+++ inc/parcelle.php
@@ -55,7 +55,7 @@ function parcelle_insert($surface,$id_variete_the) {
     }
 
     function parcelle_getByid($numero) {
-        $requete = $this->connexion->prepare("SELECT * FROM {$this->table} WHERE numero = :numero");
+        $requete = connect()->prepare("SELECT * FROM the_parcelle WHERE numero = :numero");
         $requete->bindValue(':numero', $numero);
         $requete->execute();
 
@@ -67,6 +67,6 @@ function parcelle_insert($surface,$id_variete_the) {
         return $crud->lister();
     }
 
-    var_dump(parcelle_variete_getAll());
+//    var_dump(parcelle_getByid(1));
 //    parcelle_insert(25,1);
 ?>
