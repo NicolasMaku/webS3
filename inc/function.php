235114035e34001315135c28b26dd9f52d0419c1
AndyRdn <andyrdn4@gmail.com>
2024-02-12 17:17:00 +0300

    Revert "ceuillette & parcelle & resultat"
    This reverts commit c5ea3bd331edfa20da01830978dac4b97e93c085.

--- inc/function.php
+++ inc/function.php
@@ -4,5 +4,4 @@
     require_once "Crud.php";
     require_once "ceuillette.php";
     require_once "depense.php";
-    require_once "resultat.php";
 ?>
--- inc/parcelle.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once "Crud.php";
-    include_once "base.php";
-    function getAll() {
-        $crud = new Crud('the_parcelle',connect());
-        return $crud->lister();
-    }
-?>
