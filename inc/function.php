5f675f2aaab00e37a0216b804cf3da6c8a30a827
NicolasMaku <108606635+NicolasMaku@users.noreply.github.com>
2024-02-12 21:42:35 +0300

    Reparation erreur

--- inc/function.php
+++ inc/function.php
@@ -9,6 +9,6 @@
     require_once "ceuilleur.php";
     require_once "parcelle.php";
     require_once "categorie-depense.php";
-    require_once "variete_the.php";
-    require_once "salaire";
+    require_once "variete-the.php";
+    require_once "salaire.php";
 ?>
