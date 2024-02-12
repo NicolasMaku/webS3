9e12531c6fc832da62e08a3176768d9c22a1a195
alextagNx <nathanalekskevin@gmail.com>
2024-02-12 21:49:18 +0100

    Update insererParcelle.php

--- pages/insererParcelle.php
+++ pages/insererParcelle.php
@@ -22,7 +22,7 @@
                 <div class="card mb-4">
                     <h5 class="card-header"><?php echo $is_update ? "Modifier" : "Inserer"?> une Parcelle</h5>
                     <div class="card-body">
-                        <form action="" id="parcelle-form">
+                        <form action="../controllers/parcelleControl.php" method="POST" id="parcelle-form">
                             <input type="hidden" name="action" value="<?php echo $is_update ? "update" : "save"?>">
 
                             <?php if($is_update) { ?>
