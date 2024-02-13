a98e2ea717ff63d56670a2535c088d50c8995300
alextagNx <nathanalekskevin@gmail.com>
2024-02-13 09:56:12 +0100

    mois control

--- inc/mois.php
+++ inc/mois.php
@@ -19,16 +19,18 @@
 
     function mois_reset() {
         $connection = connect();
-        $sql = "update the_mois_regenerer set regenerer=0";
+        $sql = "update the_mois_regenerer set regenerer=0 WHERE true";
         $connection->query($sql)->execute();
     }
 
     function mois_update_regenerated($id_mois) {
         $connection = connect();
-        foreach ($id_mois as $cle => $value) {
-            $sql = "update the_mois_regenerer set regenerer=1 where id=".$value;
-            $connection->query($sql)->execute();
-        }
+        $sql = "update the_mois_regenerer set regenerer=1 where id=".$id_mois;
+        $connection->query($sql)->execute();
+//        foreach ($id_mois as $cle => $value) {
+//            $sql = "update the_mois_regenerer set regenerer=1 where id=".$value;
+//            $connection->query($sql)->execute();
+//        }
     }
 
 //    $mois = [
--- pages/moisDeProduction.php
+++ pages/moisDeProduction.php
@@ -8,9 +8,11 @@
 
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="card mb-4">
-            <h5 class="card-header">Checkboxes and Radios</h5>
+            <h5 class="card-header">Regeneration automatique</h5>
             <!-- Checkboxes and Radios -->
+
             <form action="../controllers/moisControl.php" method="POST">
+                <input type="hidden" name="action" value="update">
             <div class="card-body">
                 <div class="row gy-3">
                     <div class="col-md">
@@ -18,12 +20,14 @@
 
                         <?php foreach ($liste_mois as $mois) {?>
                             <div class="form-check mt-3">
-                                <input class="form-check-input" type="checkbox" value="<?php echo $mois['id']?>" name="idMois[]" id="defaultCheck1">
+                                <input class="form-check-input" type="checkbox" value="<?php echo $mois['id']?>" <?php echo $mois['regenerer'] == 1 ? "checked" : "" ?> name="idMois[]" id="defaultCheck1">
                                 <label class="form-check-label" for="defaultCheck1"> <?php echo $mois['nom'] ?> </label>
                             </div>
                         <?php } ?>
 
                     </div>
+
+                    <input type="submit" class="btn btn-primary" value="Valider" style="background-color: green">
 
                 </div>
             </form>
