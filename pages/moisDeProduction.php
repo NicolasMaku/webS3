17a0218c2293f1c293635c6655fd836b7f539997
alextagNx <nathanalekskevin@gmail.com>
2024-02-13 09:16:51 +0100

    mois control

--- pages/moisDeProduction.php
+++ pages/moisDeProduction.php
@@ -1,6 +1,6 @@
 <?php
     include_once "../inc/function.php";
-    $liste_mois = 
+    $liste_mois = mois_getAll();
 ?>
 
 <div class="content-wrapper">
@@ -10,29 +10,19 @@
         <div class="card mb-4">
             <h5 class="card-header">Checkboxes and Radios</h5>
             <!-- Checkboxes and Radios -->
-            <form action="">
+            <form action="../controllers/moisControl.php" method="POST">
             <div class="card-body">
                 <div class="row gy-3">
                     <div class="col-md">
-                        <small class="text-light fw-medium">Checkboxes</small>
-                        <div class="form-check mt-3">
-                            <input class="form-check-input" type="checkbox" value="1" id="defaultCheck1">
-                            <label class="form-check-label" for="defaultCheck1"> Janvier </label>
-                        </div>
+                        <small class="text-light fw-medium">Choisissex les mois de regeneration automatique</small>
 
-                        <div class="form-check mt-3">
-                            <input class="form-check-input" type="checkbox" value="2" id="defaultCheck1">
-                            <label class="form-check-label" for="defaultCheck1"> Fevrier </label>
-                        </div>
-                        <div class="form-check mt-3">
-                            <input class="form-check-input" type="checkbox" value="3" id="defaultCheck1">
-                            <label class="form-check-label" for="defaultCheck1"> Mars </label>
-                        </div>
-                        <div class="form-check mt-3">
-                            <input class="form-check-input" type="checkbox" value="4" id="defaultCheck1">
-                            <label class="form-check-label" for="defaultCheck1"> Avril </label>
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Save" style="background-color: green">
+                        <?php foreach ($liste_mois as $mois) {?>
+                            <div class="form-check mt-3">
+                                <input class="form-check-input" type="checkbox" value="<?php echo $mois['id']?>" name="idMois[]" id="defaultCheck1">
+                                <label class="form-check-label" for="defaultCheck1"> <?php echo $mois['nom'] ?> </label>
+                            </div>
+                        <?php } ?>
+
                     </div>
 
                 </div>
