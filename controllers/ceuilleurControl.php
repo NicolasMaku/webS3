4b688aea5fb70a14bf54664f41a1a51065125eff
alextagNx <nathanalekskevin@gmail.com>
2024-02-13 14:41:56 +0100

    a

--- controllers/ceuilleurControl.php
+++ controllers/ceuilleurControl.php
@@ -39,12 +39,16 @@ switch ($action){
             }
             exit();
 
+
     case "salaire":
             echo salaire_getByid($_POST['idCeuilleur'])['montant'];
             exit();
 
+
     case "payement":
-            echo get
+//        echo "a";
+            echo getPayement_par_Ceuilleur($_POST['debut'], $_POST['fin']);
+//            var_dump(getPayement_par_Ceuilleur($_POST['debut'], $_POST['fin']));
 
 
 }
--- pages/payementCeuilleur.php
+++ pages/payementCeuilleur.php
@@ -1,6 +1,5 @@
 <?php
 include_once "../inc/function.php";
-//$ceuilleurs = getPayement_par_Ceuilleur("2023-01-01", <?php now ?><!--);-->
 
 if(isset($_GET['msg'])){
     alert($_GET['msg']);
@@ -19,9 +18,10 @@ if(isset($_GET['msg'])){
             <div class="table-responsive text-nowrap">
                 <form action="" id="crit-form">
                     <div class="input-group">
+                        <input type="hidden" name="action" value="payement">
                         <span class="input-group-text">Date Min and Max</span>
-                        <input type="date" aria-label="Min" class="form-control" id="date-min">
-                        <input type="date" aria-label="Max" class="form-control" id="date-max">
+                        <input type="date" aria-label="Min" class="form-control" id="date-min" name="debut" value="2023-01-01">
+                        <input type="date" aria-label="Max" class="form-control" id="date-max" name="fin" value="2024-02-14">
                     </div>
                     <br>
                     <input type="submit" class="btn btn-primary" value="Afficher">
@@ -66,7 +66,9 @@ if(isset($_GET['msg'])){
         let form = document.getElementById("crit-form");
         let table = document.getElementById("my_table");
 
-        let formData = new FormData()
+        let formData = new FormData(form);
+
+        refresh_table();
 
         function refresh_table(){
             const xhr = new XMLHttpRequest();
@@ -75,7 +77,7 @@ if(isset($_GET['msg'])){
                 if(xhr.readyState === 4) {
                     if(xhr.status === 200){
                         let newDonnees = JSON.parse(xhr.responseText);
-                        let tBody = document.getElementById("the_table");
+                        let tBody = document.getElementById("my_table");
                         tBody.innerHTML = "";
 
                         for (let i = 0; i < newDonnees.length ; i++) {
@@ -107,9 +109,11 @@ if(isset($_GET['msg'])){
 
                     }
                 }
+
             });
 
-            xhr.open("POST", "", true);
+
+            xhr.open("POST", "../controllers/ceuilleurControl.php", true);
             xhr.send(formData);
         }
     })
