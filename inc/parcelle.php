e9b2a702fa991cd2974fd4fd373a3241d344b46b
NicolasMaku <108606635+NicolasMaku@users.noreply.github.com>
2024-02-14 15:01:43 +0300

    Update parcelle.php

--- inc/parcelle.php
+++ inc/parcelle.php
@@ -1,23 +1,22 @@
 <?php
-    include_once "Crud.php";
-    include_once "base.php";
-    function parcelle_getAll() {
-        $crud = new Crud('the_parcelle',connect());
-        return $crud->lister();
-    }
+include_once "Crud.php";
+include_once "base.php";
+function parcelle_getAll() {
+    $crud = new Crud('the_parcelle',connect());
+    return $crud->lister();
+}
 
-    function parcelle_supprimer($id) {
-        $requete = connect()->prepare("DELETE FROM the_parcelle WHERE numero = :id");
-        $requete->bindValue(':id', $id);
-        $requete->execute();
-    }
+function parcelle_supprimer($id) {
+    $requete = connect()->prepare("DELETE FROM the_parcelle WHERE numero = :id");
+    $requete->bindValue(':id', $id);
+    $requete->execute();
+}
 
-function parcelle_insert($surface,$id_variete_the,$poidsInitiale) {
+function parcelle_insert($surface,$id_variete_the) {
 
     $parcelle = [
         "surface" => $surface,
         "id_variete_the" => $id_variete_the,
-        "poids_initiale" => $poidsInitiale
     ];
 
     try {
@@ -31,50 +30,54 @@ function parcelle_insert($surface,$id_variete_the,$poidsInitiale) {
     }
 }
 
-    function parcelle_modify($numero,$surface,$id_variete_the,$poidsInitiale) {
+function parcelle_modify($numero,$surface,$id_variete_the) {
 
-        $parcelle = [
-            "surface" => $surface,
-            "id_variete_the" => $id_variete_the,
-            "poids_initiale" => $poidsInitiale
-        ];
+    $parcelle = [
+        "surface" => $surface,
+        "id_variete_the" => $id_variete_the,
+    ];
 
-        try {
-            $crud = new Crud('the_parcelle',connect());
-            $modification = '';
-            foreach ($parcelle as $cle => $valeur) {
-                $modification .= "{$cle} = :{$cle}, ";
-            }
-            $modification = rtrim($modification, ', ');
-
-            $requete = connect()->prepare("UPDATE the_parcelle SET {$modification} WHERE numero = :numero");
-
-            $requete->bindValue(':numero', $numero);
-
-            foreach ($parcelle as $cle => $valeur) {
-                $requete->bindValue(':' . $cle, $valeur);
-            }
-
-            $requete->execute();
-            return true;
-
-        }catch (Exception $exception){
-            return $exception->getMessage();
+    try {
+        $crud = new Crud('the_parcelle',connect());
+        $modification = '';
+        foreach ($parcelle as $cle => $valeur) {
+            $modification .= "{$cle} = :{$cle}, ";
         }
-    }
+        $modification = rtrim($modification, ', ');
 
-    function parcelle_getByid($numero) {
-        $requete = connect()->prepare("SELECT * FROM the_parcelle WHERE numero = :numero");
+        $requete = connect()->prepare("UPDATE the_parcelle SET {$modification} WHERE numero = :numero");
+
         $requete->bindValue(':numero', $numero);
+
+        foreach ($parcelle as $cle => $valeur) {
+            $requete->bindValue(':' . $cle, $valeur);
+        }
+
         $requete->execute();
+        return true;
 
-        return $requete->fetch(PDO::FETCH_ASSOC);
+    }catch (Exception $exception){
+        return $exception->getMessage();
     }
+}
 
-    function parcelle_variete_getAll() {
-        $crud = new Crud('the_parcelle_variete',connect());
-        return $crud->lister();
-    }
+function parcelle_getByid($numero) {
+    $requete = connect()->prepare("SELECT * FROM the_parcelle WHERE numero = :numero");
+    $requete->bindValue(':numero', $numero);
+    $requete->execute();
+
+    return $requete->fetch(PDO::FETCH_ASSOC);
+}
+
+function parcelle_variete_getAll() {
+    $crud = new Crud('the_parcelle_variete',connect());
+    return $crud->lister();
+}
+
+function parcelle_getAll_rest() {
+
+}
+
 
 //    var_dump(parcelle_getByid(1));
 //    parcelle_insert(25,1);
